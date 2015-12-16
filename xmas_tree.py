e1bcdb20ec612976e5c70c6a2e504330b7a49654
aplakia <aplakia@snf-687787.(none)>
2015-12-16 10:41:47 +0200

    my tree

---
+++ xmas_tree.py
@@ -0,0 +1,12 @@
+treeheight=input ("Give the height of the tree")
+trunkheight= input ("Give the height of the trunk")
+treewidth= input ("Give the width of the trunk")
+i=0
+while i<treeheight:
+  print ((" " * (treeheight-i)) + ("*" * (i*2+1)))
+  i=i+1
+j=0
+n=round((trunkheight/2),0
+while j<trunkheight:
+  print ((" " * (trunkheight-n)+("*" * trunkwidth)))
+  j=j+1
